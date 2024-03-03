f1c99f471e9baf37bb0b6669cc002629f1b5a3c1
Ahn GyuTae <xpsj20@gmail.com>
2024-03-03 21:15:00 +0900

    ::Add On Off Switch Control::  - On-Off 스위치를 UserControl로 추가  - On-Off 스위치의 애니메이션 작업
    By GyuTae Ahn <xpsj20@gmail.com>

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
+[assembly: System.Windows.ThemeInfo(System.Windows.ResourceDictionaryLocation.None, System.Windows.ResourceDictionaryLocation.SourceAssembly)]
+
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
---
+++ WPF_xamls/Onoff_Control.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace CROFFLE_WPF.WPF_xamls
+{
+    /// <summary>
+    /// Onoff_Control.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Onoff_Control : UserControl
+    {
+
+        private bool value = false;
+
+        public bool state { get {  return value; } set { this.value = value; } }
+
+        public Onoff_Control()
+        {
+            InitializeComponent();
+            Initializing();
+        }
+
+        public void Initializing()
+        {
+            if (value)
+            {
+                switch_bd.Background = Brushes.LimeGreen;
+                changer_grid.Width = 50;
+            }
+            else
+            {
+                switch_bd.Background = Brushes.LightGray;
+                changer_grid.Width = 20;
+            }
+        }
+
+        public void Change()
+        {
+            DoubleAnimation da = new DoubleAnimation();
+
+            if (value)
+            {
+                da.From = 50;
+                da.To = 20;
+                switch_bd.Background = Brushes.LightGray;
+                value = !value;
+                Console.WriteLine(value);
+            }
+            else
+            {
+                da.From = 20;
+                da.To = 50;
+                switch_bd.Background = Brushes.LimeGreen;
+                value = !value;
+                Console.WriteLine(value);
+            }
+            da.Duration = new Duration(new TimeSpan(500000));
+
+            changer_grid.BeginAnimation(Grid.WidthProperty, da);
+        }
+
+        private void MouseClick(object sender, MouseButtonEventArgs e)
+        {
+            Change();
+        }
+    }
+}
